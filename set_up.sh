7660678983d995de3f0b99ad5a80d68f345386a1
Ubuntu <ubuntu@ip-172-31-55-118.ec2.internal>
2019-03-29 05:15:28 +0000

    Changed set up script to no longer use git hash

--- set_up.sh
+++ set_up.sh
@@ -2,43 +2,42 @@
 
 # Set up variables and local directory
 UTIL_REPO_NAME="dl_utilities"
-UTIL_REPO_GIT_HASH="6ed7c4c8e089e28a15c32e1b8dfb071d16e2c6c2"
-
-CNN_MODEL_REPO_NAME="state_of_art_cnns"
-CNN_MODEL_REPO_GIT_HASH="f6f2a22edb7a9e069e62e65740c0a2b34b0d392b"
+SOA_CNNS_REPO_NAME="start_of_art_cnns"
 
 cd `dirname $0`
 
-
 # Get necessary repo's
-echo -n "Getting other repositories needed for the project...  "
+echo -n "Getting repositories needed for the project...  "
 
-if [ ! -d $UTIL_REPO_NAME ]; then
-    git clone git@github.com:alijkhalil/"$UTIL_REPO_NAME".git > /dev/null 2>&1
-    
+# Do utilities one
+if [ ! -d ../$UTIL_REPO_NAME ]; then
+    if [ -d $UTIL_REPO_NAME ]; then
+        rm -rf $UTIL_REPO_NAME
+    fi
+
+    git clone https://github.com/alijkhalil/$UTIL_REPO_NAME.git > /dev/null 2>&1
     if [ $? -ne 0 ]; then
-        echo "Download error!"
+        echo "Download error!" >&2
         exit 1
     fi
+
+    mv $UTIL_REPO_NAME ../
 fi
 
-cd $UTIL_REPO_NAME
-git checkout $UTIL_REPO_GIT_HASH > /dev/null 2>&1
-cd - > /dev/null 2>&1
+# Do state of art CNNs one
+if [ ! -d ../$SOA_CNNS_REPO_NAME ]; then
+    if [ -d $SOA_CNNS_REPO_NAME ]; then
+        rm -rf $SOA_CNNS_REPO_NAME
+    fi
 
-if [ ! -d $CNN_MODEL_REPO_NAME ]; then
-    git clone git@github.com:alijkhalil/"$CNN_MODEL_REPO_NAME".git > /dev/null 2>&1
-    
+    git clone https://github.com/alijkhalil/$SOA_CNNS_REPO_NAME.git > /dev/null 2>&1
     if [ $? -ne 0 ]; then
-        echo "Download error!"
+        echo "Download error!" >&2
         exit 1
     fi
+
+    mv $SOA_CNNS_REPO_NAME ../
 fi
-
-cd $CNN_MODEL_REPO_NAME
-git checkout $CNN_MODEL_REPO_GIT_HASH > /dev/null 2>&1
-cd - > /dev/null 2>&1
-
 
 # Print success and exit
 echo "Done!"
